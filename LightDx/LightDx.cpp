469315bc50a93a424cdb9e061b91741cd6d6d7fc
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2022-11-18 22:21:12 -0700

    Update LightDx.cpp
    Some changes to light style calculations. Data Save / Load. Hopefully more performant.

--- LightDx/LightDx.cpp
+++ LightDx/LightDx.cpp
@@ -8,15 +8,26 @@ Register as tool class in NativeLib.cpp
 void LightDx::_register_methods()
 {
 	register_property("properties", &LightDx::set_properties, &LightDx::get_properties, {});
+	register_property("energy_1", &LightDx::set_energy_1, &LightDx::get_energy_1, 1.0f);
+	register_property("energy_2", &LightDx::set_energy_2, &LightDx::get_energy_2, 0.0f);
+	register_property("color_1", &LightDx::set_color_1, &LightDx::get_color_1, Color(1.0f, 1.0f, 1.0f));
+	register_property("color_2", &LightDx::set_color_2, &LightDx::get_color_2, Color());
+	register_property("style", &LightDx::set_style, &LightDx::get_style, String("m"));
+	register_property("style_time", &LightDx::set_style_time, &LightDx::get_style_time, 3600.0f);
 	register_method("reparent", &LightDx::reparent);
+	register_method("update_style", &LightDx::update_style);
 	register_method("trigger", &LightDx::trigger);
+	register_method("data_save", &LightDx::data_save);
+	register_method("data_load", &LightDx::data_load);
 	register_method("_ready", &LightDx::_ready);
-	register_method("_physics_process", &LightDx::_physics_process);
+	register_method("_process", &LightDx::_process);
 }
 
 void LightDx::set_properties(Dictionary new_properties)
 {
 	properties = new_properties;
+	if (!Engine::get_singleton()->is_editor_hint())
+		return;
 	// Make sure we light both the world visual layer and the view weapon layer
 	set_layer_mask(3);
 	set_cull_mask(3);
@@ -24,6 +35,7 @@ void LightDx::set_properties(Dictionary new_properties)
 	if (get_class() == "SpotLight")
 	{
 		// Spotlight direction
+		set_rotation_degrees(GameManager::demangler(properties,1));
 		if (properties.has("mangle"))
 		{
 			Vector3 m = properties["mangle"];
@@ -75,7 +87,7 @@ void LightDx::set_properties(Dictionary new_properties)
 		color_2 = properties["_color2"];
 	// Animation style - please see the style functions below
 	if (properties.has("style"))
-		set_style(properties["style"]);
+		update_style(properties["style"]);
 	// Lightmap setup
 	if (style.length() > 1 || properties.has("targetname"))
 		set_bake_mode(BAKE_DISABLED);
@@ -92,10 +104,19 @@ void LightDx::set_properties(Dictionary new_properties)
 	};
 }
 
-Dictionary LightDx::get_properties()
-{
-	return properties;
-}
+Dictionary LightDx::get_properties() { return properties; }
+void LightDx::set_style(String s) { style = s; }
+String LightDx::get_style() { return style; }
+void LightDx::set_style_time(float t) { style_time = t; }
+float LightDx::get_style_time() { return style_time; }
+void LightDx::set_energy_1(float e) { energy_1 = e; }
+float LightDx::get_energy_1() { return energy_1; }
+void LightDx::set_energy_2(float e) { energy_2 = e; }
+float LightDx::get_energy_2() { return energy_2; }
+void LightDx::set_color_1(Color c) { color_1 = c; }
+Color LightDx::get_color_1() { return color_1; }
+void LightDx::set_color_2(Color c) { color_2 = c; }
+Color LightDx::get_color_2() { return color_2; }
 
 void LightDx::reparent()
 {
@@ -116,95 +137,114 @@ void LightDx::reparent()
 		};
 }
 
-void LightDx::trigger(Node* caller)
-{
-	set_visible(!is_visible());
-}
+void LightDx::trigger(Node* caller) { set_visible(!is_visible()); }
 
 // LIGHT STYLES
-void LightDx::set_style(int style_id)
+void LightDx::update_style(int style_id)
 {
 	switch (style_id)
 	{
 	case(1):
 		// Flicker 1
 		style = "mmnmmommommnonmmonqnmmo";
-		return;
+		break;
 	case(2):
 		// Slow Strong Pulse
 		style = "abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba";
-		return;
+		break;
 	case(3):
 		// Candle 1
 		style = "mmmmmaaaaammmmmaaaaaabcdefgabcdefg";
-		return;
+		break;
 	case(4):
 		// Fast Strobe
 		style = "mamamamamama";
-		return;
+		break;
 	case(5):
 		// Gentle Pulse
 		style = "jklmnopqrstuvwxyzyxwvutsrqponmlkj";
-		return;
+		break;
 	case(6):
 		// Flicker 2
 		style = "nmonqnmomnmomomno";
-		return;
+		break;
 	case(7):
 		// Candle 2
 		style = "mmmaaaabcdefgmmmmaaaammmaamm";
-		return;
+		break;
 	case(8):
 		// Candle 3
 		style = "mmmaaammmaaammmabcdefaaaammmmabcdefmmmaaaa";
-		return;
+		break;
 	case(9):
 		// Slow Strobe
 		style = "aaaaaaaazzzzzzzz";
-		return;
+		break;
 	case(10):
 		// Flourescent Flicker
 		style = "mmamammmmammamamaaamammma";
-		return;
+		break;
 	case(11):
 		// Slow Pulse, No Fade To Black
 		style = "abcdefghijklmnopqrrqponmlkjihgfedcba";
-		return;
+		break;
 	case(63):
 		// 2nd
 		style = "a";
-		return;
+		break;
 	default:
 		// Stay at energy_1, color_1
 		style = "m";
-		return;
+		break;
 	};
+	style_vec.clear();
+	for (int i = 0; i < style.length(); i++)
+		style_vec.push_back(float(String("abcdefghijklmnopqrstuvwxyz").find(style[i])) * 0.0833333f);
 }
 
-float LightDx::style_proc(float delta)
+Dictionary LightDx::data_save()
 {
-	style_ct += delta;
-	int i = int(style_ct * 10) % style.length();
-	float t = float(String("abcdefghijklmnopqrstuvwxyz").find(style[i])) * 0.083333f;
-	return t;
+	Dictionary data;
+	data["on"] = is_visible();
+	data["style"] = style;
+	data["style_ct"] = style_ct;
+	return data;
+}
+
+void LightDx::data_load(Dictionary data)
+{
+	set_visible(data["on"]);
+	style = data["style"];
+	style_ct = data["style_ct"];
+	add_to_group("SAV");
 }
 
 void LightDx::_init(){}
 
 void LightDx::_ready()
 {
-	//if (!Engine::get_singleton()->is_editor_hint() && properties.has("targetname"))
-	//	cast_to<Game>(get_node("/root/GAME"))->set_node_targetname(this, properties["targetname"]);
+	if (!Engine::get_singleton()->is_editor_hint())
+	{
+		if (get_bake_mode() != BAKE_ALL)
+			add_to_group("SAV");
+		if (properties.has("targetname"))
+			cast_to<GameManager>(get_node("/root/GameManager"))->set_node_targetname(this, properties["targetname"]);
+		style_vec.clear();
+		for (int i = 0; i < style.length(); i++)
+			style_vec.push_back(float(String("abcdefghijklmnopqrstuvwxyz").find(style[i])) * 0.0833333f);
+	};
 }
 
-void LightDx::_physics_process(float delta)
+void LightDx::_process(float delta)
 {
 	if (style.length() > 1)
 	{
-		if (style_ct > 10800)
-			style_ct = 0.0f;
-		float t = style_proc(delta);
-		set_param(Light::PARAM_ENERGY, Math::lerp(energy_1, energy_2, t) * float(is_visible()));
-		set_color(color_1.linear_interpolate(color_2, t));
+		style_ct += delta;
+		if (style_ct >= 3600.0f)
+			style_ct -= 3600.0f;
+		int i = int(floor(style_ct * 10.0f)) % style_vec.size();
+		float t = style_vec[i];
+		set_param(Light::PARAM_ENERGY, Math::lerp(energy_2, energy_1, t));
+		set_color(color_2.linear_interpolate(color_1, t));
 	};
 }
