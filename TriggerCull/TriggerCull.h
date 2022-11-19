55fd456684f5ed62f8fa4a735d5cce33861e4145
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2022-11-18 22:35:53 -0700

    Create TriggerCull.h
    Initial commit

---
+++ TriggerCull/TriggerCull.h
@@ -0,0 +1,61 @@
+/*******************************************************************************
+TRIGGER ROOM
+Used in culling.Created in Trenchbroom using a single convex brush.
+*******************************************************************************/
+#pragma once
+#include "Godot.hpp"
+#include <ArrayMesh.hpp>
+#include <MeshDataTool.hpp>
+#include "MeshInstance.hpp"
+#include "Room.hpp"
+#include "Portal.hpp"
+#include <OccluderShapePolygon.hpp>
+#include "Occluder.hpp"
+
+class TriggerRoom : public Room
+{
+private:
+	GODOT_CLASS(TriggerRoom, Room);
+	Dictionary properties;
+public:
+	static void _register_methods();
+	void set_properties(Dictionary p);
+	Dictionary get_properties();
+	void _init();
+};
+
+/*******************************************************************************
+TRIGGER PORTAL
+Used in Room culling. Created in Trenchbroom by painting a Portal Texture on
+one face and Skip textures on all others.
+*******************************************************************************/
+class TriggerPortal : public Portal
+{
+private:
+	GODOT_CLASS(TriggerPortal, Portal);
+	Dictionary properties = {};
+public:
+	static void _register_methods();
+	void set_properties(Dictionary p);
+	Dictionary get_properties();
+	void link();
+	void _init();
+};
+
+/*******************************************************************************
+TRIGGER OCCLUDER
+Used in occlusion culling. Created in Trenchbroom by painting an Occluder Texture
+on one face and Skip textures on all others.
+*******************************************************************************/
+class TriggerOccluder : public Occluder
+{
+private:
+	GODOT_CLASS(TriggerOccluder, Occluder);
+	Dictionary properties = {};
+public:
+	static void _register_methods();
+	void set_properties(Dictionary p);
+	Dictionary get_properties();
+	void reparent();
+	void _init();
+};
