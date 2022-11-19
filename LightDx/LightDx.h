b520746bbb7b599b60a3b273bb3660bcf142ad88
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2022-11-18 22:22:51 -0700

    Update LightDx.h

--- LightDx/LightDx.h
+++ LightDx/LightDx.h
@@ -10,25 +10,30 @@ Register as a tool class in NativeLib.cpp
 #include "Light.hpp"
 //#include "Game.h"
 
-using namespace godot;
-
 class LightDx : public Light
 {
 private:
 	GODOT_CLASS(LightDx, Light);
 	Dictionary properties = {};
-	String style = "";
-	float style_ct = 0.0f, energy_1 = 1.0f, energy_2 = 0.0f;
+	String style = "m";
+	float style_ct = 0.0f, style_time = 3600.0f, energy_1 = 1.0f, energy_2 = 0.0f;
 	Color color_1 = Color(1.0f, 1.0f, 1.0f), color_2 = Color();
+	std::vector<float> style_vec = {};
 public:
 	static void _register_methods();
-	void set_properties(Dictionary new_properties);
-	Dictionary get_properties();
+	void set_properties(Dictionary new_properties);	Dictionary get_properties();
+	void set_style(String s); String get_style();
+	void set_style_time(float t); float get_style_time();
+	void set_energy_1(float e); float get_energy_1();
+	void set_energy_2(float e); float get_energy_2();
+	void set_color_1(Color c); Color get_color_1();
+	void set_color_2(Color c); Color get_color_2();
 	void reparent();
 	void trigger(Node* caller);
-	void set_style(int style_id);
-	float style_proc(float delta);
+	void update_style(int style_id);
+	Dictionary data_save();
+	void data_load(Dictionary data);
 	void _init();
 	void _ready();
-	void _physics_process(float delta);
+	void _process(float delta);
 };
