910c2307eaeb968c925122eac2cd334128051ce0
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2022-12-22 17:33:15 -0700

    Create Common.h

---
+++ Common.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Godot.hpp"
+#include <Math.hpp>
+#include <algorithm>
+#include "Engine.hpp"
+#include "SceneTree.hpp"
+#include <ResourceLoader.hpp>
+#include <PackedScene.hpp>
+#include <RandomNumberGenerator.hpp>
+#include <AudioStreamSample.hpp>
+#include <AudioStreamOGGVorbis.hpp>
+#include <Mesh.hpp>
+#include "Node.hpp"
+#include "AudioStreamPlayer.hpp"
+#include "AudioStreamPlayer3D.hpp"
+
+//using namespace std;
+using namespace godot;
