5992de1e0fe7ec9e9036adecaf4a31fda0657bb9
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2023-09-26 12:00:29 -0400

    Add files via upload

---
+++ GDScript 2.0/func_illusionary.gd
@@ -0,0 +1,56 @@
+@tool
+extends Node3D
+class_name FuncIllusionary
+
+const INVERSE_SCALE: float = 1.0 / 32.0
+
+@export var properties: Dictionary = {} :
+	set(value):
+		properties = value;
+		
+		if !Engine.is_editor_hint():
+			return
+		
+		if properties.has("flags"):
+			flags = properties["flags"] as int
+		
+		for child in get_children():
+			if child.get_class() == "MeshInstance3D":
+				var m: MeshInstance3D = child
+				m.set_gi_mode(GeometryInstance3D.GI_MODE_STATIC);
+				if properties.has("shadowcast"):
+					m.set_cast_shadows_setting((properties["shadowcast"] as GeometryInstance3D.ShadowCastingSetting))
+				else:
+					m.set_cast_shadows_setting(GeometryInstance3D.SHADOW_CASTING_SETTING_ON)
+				if m.mesh.get_class() == "ArrayMesh":
+					(m.mesh as ArrayMesh).lightmap_unwrap(Transform3D(), INVERSE_SCALE)
+				if (flags & FuncFlags.NO_CULL):
+					m.ignore_occlusion_culling = true
+		
+		if properties.has("moveparent"):
+			call_deferred("reparent_to_func_move")
+	get:
+		return properties
+
+enum FuncFlags {
+	NO_CULL = (1 << 0)
+}
+@export var flags: int = 0
+
+func reparent_to_func_move()->void:
+	var moveparent: String = properties["moveparent"]
+	moveparent = "entity_func_move_" + moveparent
+	if get_parent().get_name() == moveparent:
+		return
+	if is_inside_tree():
+		if get_parent().has_node(NodePath(moveparent)):
+			var t: Transform3D = global_transform
+			var new_parent: Node = get_parent().get_node(NodePath(moveparent));
+			get_parent().remove_child(self);
+			new_parent.add_child(self);
+			set_global_transform(t);
+			set_owner(new_parent.owner);
+			for child in get_children():
+				child.owner = owner
+				if child.get_class() == "MeshInstance3D":
+					(child as MeshInstance3D).set_ignore_occlusion_culling(true)
