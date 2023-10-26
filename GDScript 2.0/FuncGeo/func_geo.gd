c92ff5d62b5403f0d4cd46944cb4e1a6cc0194f3
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2023-10-25 23:27:11 -0400

    Update func_geo.gd

--- GDScript 2.0/FuncGeo/func_geo.gd
+++ GDScript 2.0/FuncGeo/func_geo.gd
@@ -1,12 +1,13 @@
+#*******************************************************************************
+# FUNC GEO
+#
 # Geometry mesh. Used to split up the map geometry for better culling. Best when combined with Skip textures
 # to cull the outside faces of geometry to reduce overdraw and provide better editor visibility.
 # The FGD resource defaults to Convex Collision, but Concave works quite well in conjunction with smart use of Skip.
-
+#*******************************************************************************
 @tool
-extends StaticBody3D
 class_name FuncGeo
-
-const INVERSE_SCALE: float = 1.0 / 32.0
+extends StaticBody3D
 
 @export var properties: Dictionary = {} :
 	set(value):
@@ -15,28 +16,28 @@ const INVERSE_SCALE: float = 1.0 / 32.0
 		if !Engine.is_editor_hint():
 			return
 		
+		if properties.has("appearance_flags"):
+			appearance_flags = properties["appearance_flags"] as int
 		if properties.has("flags"):
 			flags = properties["flags"] as int
-		
+		if properties.has("grav_type"):
+			grav_type = properties["grav_type"] as int		
 		for child in get_children():
-			if child.get_class() == "CollisionShape3D":
-				(child as CollisionShape3D).shape.margin = 0.01
-				
-			if child.get_class() == "MeshInstance3D":
-				var m: MeshInstance3D = child
-				m.set_gi_mode(GeometryInstance3D.GI_MODE_STATIC);
-				if properties.has("shadowcast"):
-					m.set_cast_shadows_setting((properties["shadowcast"] as GeometryInstance3D.ShadowCastingSetting))
-				else:
-					m.set_cast_shadows_setting(GeometryInstance3D.SHADOW_CASTING_SETTING_ON)
-				if m.mesh.get_class() == "ArrayMesh":
-					(m.mesh as ArrayMesh).lightmap_unwrap(Transform3D(), INVERSE_SCALE)
-				if (flags & FuncFlags.NO_CULL):
-					m.ignore_occlusion_culling = true
-	get:
-		return properties
+			if child is MeshInstance3D:
+				if (flags & Func.FUNC_FLAG_NO_CULL):
+					child.ignore_occlusion_culling = true
+				if properties.has("cast_shadow"):
+					child.cast_shadow = properties["cast_shadow"] as int
 
-enum FuncFlags {
-	NO_CULL = (1 << 0)
-}
+@export var appearance_flags: int = 0
 @export var flags: int = 0
+@export var grav_type: int = 0
+
+func _init() -> void:
+	add_to_group("WORLD")
+	add_to_group("FUNC_GEO")
+
+func _ready() -> void:
+	if !Engine.is_editor_hint():
+		if GAME.appearance_check(appearance_flags):
+			queue_free()
